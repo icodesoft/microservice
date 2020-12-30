598fc19c93a9045ed753ab579de5fd3ee1accb5f
Gary Gao(c) <ggary@vmware.com>
2020-12-30 14:30:04 +0800

    服务生产者

---
+++ service-producter/src/main/java/com/icodesoft/serviceproducter/ServiceProductApplication.java
@@ -0,0 +1,15 @@
+package com.icodesoft.serviceproducter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProductApplication.class, args);
+    }
+
+}
